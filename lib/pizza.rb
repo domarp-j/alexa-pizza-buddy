e445c4884b7830224eac26f7b4cbbd7ad5f08a09
Pramod Jacob <psjacob68@gmail.com>
2018-04-15 18:19:43 -0400

    add some intents

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,3 +1,14 @@
 class Pizza
   SIZES = [:large, :medium, :small]
+  TOPPINGS = [:mushrooms, :barbecue_sauce, :sausage, :pineapple, :ham, :garlic, :olives, :pepperoni]
+
+  def self.disallowed_toppings(toppings)
+    toppings.reject { |topping| allowed_topping?(topping) }
+  end
+
+  private
+
+  def self.allowed_topping?(topping)
+    TOPPINGS.include?(topping.gsub(' ', '_').to_sym)
+  end
 end
